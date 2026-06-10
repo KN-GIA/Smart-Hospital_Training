a64e3368ed0ca41022ea91ad1891986f600848e9
KN_GIA <anggia.fitrisya@kodingnext.com>
2026-06-11 02:12:36 +0700

    Update Hospital_App.py

--- Hospital_App.py
+++ Hospital_App.py
@@ -5,14 +5,14 @@ import os
 import numpy as np
 
 
-st.set_page_config(page_title = "Smart Hospital Patient Navigator", page_icon = " 🏥 ", layout = "wide")
+st.set_page_config(page_title = "Smart Hospital Patient Navigator", page_icon = "🏥", layout = "wide")
 
 st.markdown("""
   <style> 
-  @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap');
 
-  html, body, [class*="css"] {font-family: Inter, sans-serif}
-  #MainMenu {visibility: hideen;}
-  header[data-testid= "stheader"] {display=none;}
+    html, body, [class*="css"] {font-family: Inter, sans-serif}
+    #MainMenu {visibility: hideen;}
+    header[data-testid= "stheader"] {display=none;}
   </style>
 """)
