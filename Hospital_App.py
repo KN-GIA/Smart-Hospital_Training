548738dab3bb533db87e6c0f83b785d071f2b54a
KN_GIA <anggia.fitrisya@kodingnext.com>
2026-06-11 02:28:47 +0700

    Update Hospital_App.py

--- Hospital_App.py
+++ Hospital_App.py
@@ -14,5 +14,25 @@ st.markdown("""
     html, body, [class*="css"] {font-family: Inter, sans-serif}
     #MainMenu {visibility: hideen;}
     header[data-testid= "stheader"] {display=none;}
+    .stDeployButton {display:none;}
+    footer {visibility: hidden;}
+    .block-container {padding-top: 0 !important; padding-bottom: 2rem; !important; max-width: 1100px !important;}
+    div[data-testing="stForm"] {border:none; padding:0}
+
+    div.stButton > Button {
+    background: linear-gradient(135deg, #1a56db, #1e429f) !important;
+    color: white !important; border: none !important;
+    border-radius: 12px !important; padding: 0.75rem 2rem !important;
+    font-size: 16px !important; letter-spacing: 0.02em !important;
+    box-shadow: 0 4px 14px rgba(26,86,219,0.35)!important;
+
+    div.stButton > button:hover {background: linear-gradient(135deg, #1e429f, #1a56db) !important; }
+
+    div[data-testid="stCheckbox"] label {
+    font-size: 14px !important; font-weight: 500 !important; color:#374151 !important;
+    }
+  }
+    
+    
   </style>
-""")
+""", unsafe_allow_html=True)
