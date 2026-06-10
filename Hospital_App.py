5ed216ead51830076067f24a16a55cbf7fd800fd
KN_GIA <anggia.fitrisya@kodingnext.com>
2026-06-11 01:53:02 +0700

    Update Hospital_App.py

--- Hospital_App.py
+++ Hospital_App.py
@@ -5,6 +5,6 @@ import os
 import numpy as np
 
 
-st.set_page_config(page_title = "Smart Hospital Patient Navigator", page_icon = " 🏥 ", layout = "wide"]
+st.set_page_config(page_title = "Smart Hospital Patient Navigator", page_icon = " 🏥 ", layout = "wide")
 
 st.title("Smart Hospital Navigator")
