1dcd976101e84e560d35d764157e2a68d511bc11
KN_GIA <anggia.fitrisya@kodingnext.com>
2026-06-11 02:10:23 +0700

    Update Hospital_App.py

--- Hospital_App.py
+++ Hospital_App.py
@@ -12,7 +12,7 @@ st.markdown("""
   @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap');
 
   html, body, [class*="css"] {font-family: Inter, sans-serif}
-  #MainMenu {visibility: hidden;}
+  #MainMenu {visibility: hideen;}
   header[data-testid= "stheader"] {display=none;}
   </style>
 """)
