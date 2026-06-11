81b2fc04869791d2e2513aefbf67b8edb0724e6e
KN_GIA <anggia.fitrisya@kodingnext.com>
2026-06-11 23:25:45 +0700

    Update Hospital_App.py

--- Hospital_App.py
+++ Hospital_App.py
@@ -197,7 +197,7 @@ with st.form("triage_form"):
     </div>
     """, unsafe_allow_html=True)
 
-    ch1, ch2, ch3, _ = st.columns(4)
+    ch1, _, ch2,_, ch3, = st.columns(5)
     with ch1: hypertension  = st.checkbox("🩺 High Blood Pressure")
     with ch2: heart_disease = st.checkbox("❤️ Heart Disease")
     with ch3: asthma        = st.checkbox("💨 Asthma")
